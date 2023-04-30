5c3f14499ac5e36fb10174a395c36939abd8d745
goodlifeinc <genata93@gmail.com>
2023-04-30 10:31:25 +0300

    softuni-homes plugin with CPT initial

---
+++ wp-content/plugins/softuni-homes/softuni-homes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: SoftUni Homes
+ * Version: 0.0.1
+ * Author: Evgeni Atanasov
+ * Author URI: mailto:genata93@gmail.com
+ * Description: The SoftUni Homes plugin
+ */
+
+require 'cpt-homes.php';
+require 'functions.php';
